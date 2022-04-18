73e84cb6e2a966ec49924e930ea8280a59fa3363
karolyim <matej.karolyi@gmail.com>
2022-04-18 12:21:40 +0200

    Init project

---
+++ main.js
@@ -0,0 +1,36 @@
+// do proměnné si uložíme prvek ve stránce,
+// kam budeme vypisovat výsledek
+
+// ihned po načtení stránky zavoláme funkci,
+// která přečte hodnotu z Local Storage a zobrazí ji
+
+// funkce pro načtení a zobrazení hodnoty z Local Storage
+function nactiHodnotu() {
+	// načteme z Local Storage
+
+	// pokud hodnota nexistuje, vypíšeme chybu
+		
+	// jinak hodnota existuje a vypíšeme ji
+
+}
+
+// funkce pro uložení hodnoty do Local Storage
+function ulozHodnotu() {
+	// hodnotu načteme z pole formuláře
+
+	// a uložíme do local storage pod jménem "mojehodnota"
+
+	// a hned zavoláme funkci pro zobrazení hodnoty,
+	// aby se změna v Local Storage promítla i na stránce
+
+}
+
+// funkce pro smazání hodnoty z Local Storage
+function smazatHodnotu() {
+	// smažeme hodnotu uloženou pod jménem "mojehodnota",
+	// pokud takový klíč neexistuje, nestane se nic
+
+	// a hned zavoláme funkci pro zobrazení hodnoty,
+	// aby se změna v Local Storage promítla i na stránce
+
+}
